d93bc0c8ecdd820e806843c619477a3bd9c53986
Ndpnt-assitant <Ndpnt-assitant@users.noreply.github.com>
2024-04-26 13:23:25 +0000

    Customise country-template through CI

--- openfisca_democratic_republic_of_the_congo/__init__.py
+++ openfisca_democratic_republic_of_the_congo/__init__.py
@@ -12,8 +12,8 @@ import os
 
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 
-from openfisca_country_template import entities
-from openfisca_country_template.situation_examples import couple
+from openfisca_democratic_republic_of_the_congo import entities
+from openfisca_democratic_republic_of_the_congo.situation_examples import couple
 
 
 COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
